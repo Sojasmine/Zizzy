19c9fd5bc6db911e89462180d5ccbc1e1cf4c5e9
Sojasmine <sojasmine.gjerstad@gmail.com>
2021-08-09 15:09:24 +0000

    Added basic checkout functionality

--- checkout/migrations/0001_initial.py
+++ checkout/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Order',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('order_number', models.CharField(editable=False, max_length=32)),
                 ('full_name', models.CharField(max_length=50)),
                 ('email', models.EmailField(max_length=254)),
@@ -27,7 +27,7 @@ class Migration(migrations.Migration):
                 ('street_address1', models.CharField(max_length=80)),
                 ('street_address2', models.CharField(blank=True, max_length=80, null=True)),
                 ('county', models.CharField(blank=True, max_length=80, null=True)),
-                ('date', models.DateField(auto_now_add=True)),
+                ('date', models.DateTimeField(auto_now_add=True)),
                 ('delivery_cost', models.DecimalField(decimal_places=2, default=0, max_digits=6)),
                 ('order_total', models.DecimalField(decimal_places=2, default=0, max_digits=10)),
                 ('grand_total', models.DecimalField(decimal_places=2, default=0, max_digits=10)),
@@ -36,12 +36,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='OrderLineItem',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('product_size', models.CharField(blank=True, max_length=2)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('product_size', models.CharField(blank=True, max_length=2, null=True)),
                 ('quantity', models.IntegerField(default=0)),
-                ('lineItem_total', models.DecimalField(decimal_places=2, editable=False, max_digits=6)),
-                ('order', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='lineitems', to='checkout.order')),
-                ('product', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='products.product')),
+                ('lineitem_total', models.DecimalField(decimal_places=2, editable=False, max_digits=6)),
+                ('order', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='lineitems', to='checkout.Order')),
+                ('product', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='products.Product')),
             ],
         ),
     ]
